d416f6411554e9e919dba886c466c15c2c4cb656
Raghul GitHub <raghuljohn90@gmail.com>
2026-06-11 02:18:15 +0530

    changed score.js to work with android and removed desktop integration

--- android/game/js/score.js
+++ android/game/js/score.js
@@ -1,51 +1,15 @@
 const ScoreManager = (() => {
 
-
-  const IS_DESKTOP = !!(window.qt && window.qt.webChannelTransport);
-  let _bridge  = null;
-  let _ready   = false;
-  let _pending = [];
-
-  function _connectBridge(callback) {
-    if (!IS_DESKTOP) { callback(); return; }
-    new QWebChannel(window.qt.webChannelTransport, (channel) => {
-      _bridge = channel.objects.pyBridge;
-      _ready  = true;
-      _pending.forEach(([mode, val]) => _bridge.saveScore(mode, val));
-      _pending = [];
-      callback();
-    });
-  }
-
   function loadAll(callback) {
-    if (!IS_DESKTOP) {
-      callback({
-        easy:   parseInt(localStorage.getItem('sfHs_easy')   || '0'),
-        medium: parseInt(localStorage.getItem('sfHs_medium') || '0'),
-        hard:   parseInt(localStorage.getItem('sfHs_hard')   || '0'),
-      });
-      return;
-    }
-    _connectBridge(() => {
-      _bridge.getScores((jsonStr) => {
-        try { callback(JSON.parse(jsonStr)); }
-        catch(e) { callback({ easy: 0, medium: 0, hard: 0 }); }
-      });
+    callback({
+      easy:   parseInt(localStorage.getItem('sfHs_easy')   || '0'),
+      medium: parseInt(localStorage.getItem('sfHs_medium') || '0'),
+      hard:   parseInt(localStorage.getItem('sfHs_hard')   || '0'),
     });
   }
 
   function saveOne(mode, value) {
-
     localStorage.setItem('sfHs_' + mode, String(value));
-
-    if (IS_DESKTOP) {
-      if (_ready && _bridge) {
-        _bridge.saveScore(mode, value);
-      } else {
-        _pending.push([mode, value]);
-        if (!_bridge) _connectBridge(() => {});
-      }
-    }
   }
 
   function resetAll() {
@@ -54,5 +18,6 @@ const ScoreManager = (() => {
     });
   }
 
-  return { loadAll, saveOne, resetAll, IS_DESKTOP };
+  return { loadAll, saveOne, resetAll };
+
 })();
