2dd88cca01732735695d16aa79375dd7056a5ef3
Raghul GitHub <raghuljohn90@gmail.com>
2026-07-29 02:40:48 +0530

    updaing gobal state in reset HS function

--- web/js/script.js
+++ web/js/script.js
@@ -89,6 +89,7 @@ function togglePause() {
 function resetHS() {
   ScoreManager.resetScore(G.mode);
   document.getElementById('hv-hs').textContent = '0';
+  G.hs[G.mode] = 0;
 }
 function spawnScorePop(txt, hexColor, canvasPixelX, canvasPixelY) {
   const area  = document.getElementById('game-area');
